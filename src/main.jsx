76c41f5dd5d4c39a73101c420b97d270ea4ee515
nauyusimi <nauyusimi@gmail.com>
2025-04-03 08:56:24 +0700

    react router test

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter, Route, Routes } from 'react-router'
+import { BrowserRouter, HashRouter, Route, Routes } from 'react-router'
 import About from './pages/About.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
+
     {/* <StrictMode>
       <App />
-    </StrictMode>, */}
+      </StrictMode>, */}
     <Routes>
       <Route path="/my-portfolio/" element={<App />} />
       <Route path="/my-portfolio/about" element={<About />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 )
